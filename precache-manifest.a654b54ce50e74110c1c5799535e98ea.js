6ae680d55fc92c85ddb6b6c79367d240a5b0eb23
ankit tiwari <anitiwari95@gmail.com>
2020-10-04 11:10:11 +0530

    Updates

---
+++ precache-manifest.a654b54ce50e74110c1c5799535e98ea.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb37b431518f603a4b5f7fa1ea894309",
+    "url": "/reactweb-master/index.html"
+  },
+  {
+    "revision": "cb7bd3f7bf14631d295a",
+    "url": "/reactweb-master/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "d7076a15d4c5ea21fded",
+    "url": "/reactweb-master/static/css/main.0f9b23b0.chunk.css"
+  },
+  {
+    "revision": "cb7bd3f7bf14631d295a",
+    "url": "/reactweb-master/static/js/2.717f1fdb.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactweb-master/static/js/2.717f1fdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7076a15d4c5ea21fded",
+    "url": "/reactweb-master/static/js/main.f88c847e.chunk.js"
+  },
+  {
+    "revision": "5a73129ebe172cf2ca9b",
+    "url": "/reactweb-master/static/js/runtime-main.efeb5c2d.js"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/reactweb-master/static/media/android.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/reactweb-master/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "ac8f6f9ad22a8902b83e21560b95dca5",
+    "url": "/reactweb-master/static/media/digital.ac8f6f9a.jpeg"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/reactweb-master/static/media/hero-img.619f0cc8.png"
+  },
+  {
+    "revision": "928beca9c0e23718fe3c48a1fc8389d3",
+    "url": "/reactweb-master/static/media/img2.928beca9.svg"
+  },
+  {
+    "revision": "2eed95a2d555c404f1f451730eaa9eef",
+    "url": "/reactweb-master/static/media/marketing.2eed95a2.jpg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/reactweb-master/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/reactweb-master/static/media/software.43049cae.jpeg"
+  }
+]);
